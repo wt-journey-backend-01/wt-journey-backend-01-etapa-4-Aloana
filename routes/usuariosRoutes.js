0578458fe0fc6bc5f6a0345526ae864d50eed8c3
Aloana <aloana16neto@hotmail.com>
2025-08-24 17:45:08 -0300

    implementando a rota getMe

--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -6,7 +6,6 @@ const authMiddleware = require('../middlewares/authMiddleware');
 router.use(authMiddleware);
 
 router.get('/me', usuariosController.getMe);
-
 router.delete('/:id', usuariosController.deleteUser);
 
 module.exports = router;
